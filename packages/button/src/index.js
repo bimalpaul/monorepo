1ac5a8459d8cee92eb7c6b27d640e7c376641886
Bimal Paul <bimalbpaul@outlook.com>
2019-07-10 12:08:10 -0700

    feat: Initial push

---
+++ packages/button/src/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    background: yellow;
+    color: red;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 12px;
+    font-weight: 300;
+    padding: 9px 36px;        
+`;
+
+export default Button;
