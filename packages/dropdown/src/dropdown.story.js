65027a3cfb3499604b2a95a4d6d3abcad2164a31
Bimal Paul <bimalbpaul@outlook.com>
2019-07-10 15:42:55 -0700

    chore(readability): Updating to improve readability

---
+++ packages/dropdown/src/dropdown.story.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import Dropdown from '.';
+
+storiesOf('Dropdown', module).add('default', () => <Dropdown />);
---
+++ packages/dropdown/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dropdown = () => (
+    <select>
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+    </select>
+);
+
+export default Dropdown;
